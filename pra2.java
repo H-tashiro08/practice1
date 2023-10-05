929b39f0bf4dd75d5148748004df61f2c729b509
Tahono08 <h.tashiro.sys23@morijyobi.ac.jp>
2023-10-05 10:37:52 +0900

    pra2 javaの変更
    pra2.javaの出力を100回繰り返すプログラムに変更しました。

--- pra2.java
+++ pra2.java
@@ -1,8 +1,13 @@
 public class pra2 {
     public static void main(String args[]) {
 
+
+	      int i = 1;
+            while(i <= 101){
+
 System.out.println("Hello Would");
+i = i + 1;
 
-	    }
-}
-
+	    		}
+		}
+	}
